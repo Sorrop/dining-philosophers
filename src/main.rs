469f9133d976d183fe7897c98b4889c1c0f94bb6
Chris Klaoudianis <chris.klaou@gmail.com>
2025-09-04 14:48:20 +0000

    Make eating conditional on hunger, add statistics collection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use core::time;
-use std::{sync::{Arc, Mutex}, thread};
-
+use std::{sync::{Arc, Mutex}, thread, time::{Duration, SystemTime}};
 use rand::Rng;
 
 #[derive(Debug)]
@@ -19,6 +18,7 @@ struct Philosopher {
     id: usize,
     left_chopstick: Arc<Mutex<Chopstick>>,
     right_chopstick: Arc<Mutex<Chopstick>>,
+    times_fed: Arc<Mutex<u64>>
 }
 
 impl Philosopher {
@@ -29,27 +29,28 @@ impl Philosopher {
     ) -> Philosopher {
         Philosopher {
             id,
-            left_chopstick: left_chopstick,
-            right_chopstick: right_chopstick,
+            left_chopstick,
+            right_chopstick,
+            times_fed: Arc::new(Mutex::new(0))
         }
     }
 
     fn think(&self) {
         let mut rng = rand::rng();
-        let interval = rng.random_range(1..=15000);
+        let interval = rng.random_range(1..=5000);
         let millis = time::Duration::from_millis(interval);
-        println!("Philosopher {} thinking...", self.id);
+        println!("{} is thinking...", self.id);
         thread::sleep(millis)
     }
 
-    fn eat(&self) {
+    fn eat(&self, left_id: usize, right_id: usize) {
         let mut rng = rand::rng();
-        let interval = rng.random_range(1..=15000);
+        let interval = rng.random_range(1..=5000);
         let millis = time::Duration::from_millis(interval);
-        println!("Philosopher {:?} eating with {:?} and {:?}",
+        println!("{} is eating with {:?} and {:?}",
                  self.id,
-                 self.left_chopstick,
-                 self.right_chopstick);
+                 left_id,
+                 right_id);
         thread::sleep(millis)
     }
 
@@ -58,9 +59,12 @@ impl Philosopher {
         let locked_right = self.right_chopstick.try_lock();
         if let Ok(left_guard) = locked_left {
             if let Ok(right_guard) = locked_right {
-                self.eat();
+                self.eat(left_guard.id, right_guard.id);
+                let mut t = self.times_fed.lock().unwrap();
+                *t += 1;
                 drop(right_guard);
                 drop(left_guard);
+                println!("{} finished eating", self.id);
                 return true
             }
         }
@@ -76,26 +80,53 @@ fn n_chopsticks(n: usize) -> Vec<Arc<Mutex<Chopstick>>> {
     out
 }
 
+fn is_hungry() -> bool {
+    let mut rng = rand::rng();
+    rng.random_bool(0.5)
+}
+
 fn main() {
     let n: usize = 5;
     let chopsticks = n_chopsticks(n);
     let mut philosophers: Vec<Philosopher> = Vec::new();
     for i in 0..n {
-        philosophers.push(Philosopher::new(
-            i,
-            chopsticks[(n - 1 + i) % n].clone(),
-            chopsticks[i].clone(),
-        ));
+        philosophers.push(
+            Philosopher::new(
+                i,
+                chopsticks[(n - 1 + i) % n].clone(),
+                chopsticks[i].clone(),
+            ));
     }
+
+    let stats: Vec<_> = philosophers.iter().map(|p| (p.id, p.times_fed.clone())).collect();
+
+    let timeout = Duration::new(60, 0);
+    let now = SystemTime::now();
 
     thread::scope (|scope| {
         for p in philosophers {
             scope.spawn(move || {
                 loop {
+                    if let Ok(elapsed) = now.elapsed() {
+                        if elapsed >= timeout {
+                            break;
+                        }
+                    } else {
+                        eprintln!("Error getting system time");
+                        break;
+                    }
                     p.think();
-                    p.try_to_eat();
+                    if is_hungry() {
+                        println!("{} is trying to eat", p.id);
+                        p.try_to_eat();
+                    }
                 }
             });
         }
     });
+
+    println!("--------- Results ---------");
+    for (id, times_fed) in stats {
+        println!("{} ate {} times", id, times_fed.lock().unwrap());
+    }
 }
